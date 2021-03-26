7d7fc162e44f5f3b42cb2d02c798b579887c19ce
OpenFlowAdmin <OpenFlowAutomation@gmail.com>
2021-03-26 18:13:42 +0000

    Simplified PluginFront to the new system

--- src/Plugins/OpenFlow_Inbuilt/Plugin_Front.cs
+++ src/Plugins/OpenFlow_Inbuilt/Plugin_Front.cs
@@ -22,16 +22,6 @@
 
     public class Plugin_Front : IPlugin
     {
-        private readonly IEnumerable<KeyValuePair<string, IDataTemplate>> editors = new Dictionary<string, IDataTemplate>();
-        private readonly IEnumerable<KeyValuePair<string, IDataTemplate>> displays = new Dictionary<string, IDataTemplate>();
-
-        public Plugin_Front()
-        {
-            string projectFolder = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.Parent.FullName, @"Plugins\OpenFlow_Inbuilt\");
-            editors = DataTemplatesFromFolder(Path.Combine(projectFolder, @"Editors"));
-            displays = DataTemplatesFromFolder(Path.Combine(projectFolder, @"Displays"));
-        }
-
         public void Register(IPluginHost host)
         {
             host.RegisterType<double>("#FF0000", "Number", 0.0, "NumberEditor", "StringDisplay");
@@ -39,17 +29,6 @@
             host.RegisterType<bool>("#00FFFF", "Condition", false, "StringDisplay", "StringDisplay");
             host.RegisterType<Action>("00FF00", "Button", null, "DefaultDisplay", "ActionDisplay");
             host.RegisterType<MouseButtonEnum>("#FFFF00", "Mouse Button", MouseButtonEnum.LeftButton, "EnumEditor", "StringDisplay");
-
-            //host.TryAddTypeConverter<double, string, Node_Convert_To_String>();
-
-            foreach (KeyValuePair<string, IDataTemplate> kvp in editors)
-            {
-                host.RegisterEditor(kvp.Key, kvp.Value);
-            }
-            foreach (KeyValuePair<string, IDataTemplate> kvp in displays)
-            {
-                host.RegisterDisplay(kvp.Key, kvp.Value);
-            }
 
             host.RegisterDisplay<IControl>("DefaultDisplay", typeof(DefaultDisplay));
             host.RegisterDisplay<IControl>("StringDisplay", typeof(StringDisplay));
@@ -63,20 +42,6 @@
             host.AddNodeToMenu<Node_Join_Strings>("Text");
             host.AddNodeToMenu<FlowSwitch>("Flow Control");
             host.AddNodeToMenu<MouseButton>("Input", "Mouse");
-        }
-
-        private static IEnumerable<KeyValuePair<string, IDataTemplate>> DataTemplatesFromFolder(string folderPath)
-        {
-            IEnumerable<KeyValuePair<string, IDataTemplate>> dataTemplates = new Dictionary<string, IDataTemplate> ();
-            foreach (Styles fileStyles in Directory.EnumerateFiles(folderPath)
-                .Select(x => AvaloniaRuntimeXamlLoader.Parse(File.ReadAllText(x)) as Styles)
-                .Where(x => x != null))
-            {
-                dataTemplates = dataTemplates.Concat(fileStyles.Resources
-                    .Where(x => x.Key is string && x.Value is IDataTemplate)
-                    .Select(x => new KeyValuePair<string, IDataTemplate>(x.Key as string, x.Value as IDataTemplate)));
-            }
-            return dataTemplates;
         }
     }
 }
